9c06defbb3405d6b574aeaf5f72fa5198bd85ee1
David Vaughan <dvaug86@gmail.com>
2024-01-23 08:04:09 -0500

    start of switch statements

--- Ternery.java
+++ Ternery.java
@@ -2,11 +2,11 @@ public class Ternery {
     public static void main(String[] args) {
         int n = 4;
         int result = 0;
-        if (n % 2 == 0)
-            result = 10;
-        else
-            result = 20;
-
+        // if (n % 2 == 0)
+        //     result = 10;
+        // else
+        //     result = 20;
+result = n%2 ==0 ? 10:20;
         System.out.println(result);
     }
 }

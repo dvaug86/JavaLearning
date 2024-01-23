37a4f8896500deb24d1fb4e884a91ef06b7cde4d
David Vaughan <dvaug86@gmail.com>
2024-01-22 20:49:03 -0500

    terenery operators demo

--- IfElseDemo.java
+++ IfElseDemo.java
@@ -6,13 +6,27 @@ public class IfElseDemo {
         // else
         // System.out.println("Bye");
 
+        // int x = 5;
+        // int y = 7;
+
+        // if (x > y){
+        // System.out.print(x);
+        // System.out.print("x is greater than y");
+        // }
+        // else
+        // System.out.print("y is greater than x");
+        ///////////////////////////////////////////////////////////////////////
+
         int x = 5;
         int y = 7;
+        int z = 6;
 
-        if (x > y)
+        if (x > y && x > z)
             System.out.print(x);
-        else
+        else if (y > x && y > z) //save time by saying else if (y>z) because if first statement is false we don't need to know that y is > x
             System.out.print(y);
+        else
+            System.out.println(z);
 
     }
 }

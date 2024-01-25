5e9162f6b090f9841b1e10a0dba17c3a46cc84bd
David Vaughan <dvaug86@gmail.com>
2024-01-25 12:30:12 -0500

    do while loop

---
+++ DoWhileLoop.java
@@ -0,0 +1,9 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int i = 5;
+        {
+            System.out.println("hi " + i);
+            i++;
+        } while (i <= 4);
+    }
+}
